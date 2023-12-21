adf51e886d1628ad89d07b5f3154325ad7927516
Senku89 <148382187+Senku89@users.noreply.github.com>
2023-12-21 22:47:03 +0100

    Correction attribut listQuestions en minuscule

--- src/backend/minijeu/JeuQuiz.java
+++ src/backend/minijeu/JeuQuiz.java
@@ -6,23 +6,23 @@ import backend.donnees.Question;
 import java.util.List;
 
 public class JeuQuiz extends Minijeu {
-   List<Question> ListQuestions;
+   List<Question> listQuestions;
 
     // Constructeur pour initialiser le JeuQuiz avec les données dans le fichier CSV
     public JeuQuiz(String filePath) {
-        ListQuestions = CSVReader.readCSVQuestion(filePath);
+        listQuestions = CSVReader.readCSVQuestion(filePath);
     }
 
     // Methode pour verifier la reponse si est correcte
     public boolean checkReponse(String reponse, int questionIndex) {
         // S'assurer qu'on est avec la bonne indice
-        if (questionIndex < 0 || questionIndex >= ListQuestions.size()) {
+        if (questionIndex < 0 || questionIndex >= listQuestions.size()) {
             System.err.println("Index de la question invalide");
             return false;
         }
 
         // Obtenir la reponse correcte qui est la derniere
-        String reponseCorrecte = ListQuestions.get(questionIndex).getReponse().get(3);
+        String reponseCorrecte = listQuestions.get(questionIndex).getReponse().get(3);
 
         // Comparer la bonne reponse avec celui passée en parametre (comparaison case-sensitive)
         return reponse.equals(reponseCorrecte);
