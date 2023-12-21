65cab0b1d7a206f75e1d7dbc072a56ad8b2ef97e
Senku89 <148382187+Senku89@users.noreply.github.com>
2023-12-21 22:39:48 +0100

    JeuPassword Liste, Constructeur et Methode testPasswordLevel

--- src/backend/minijeu/JeuPassword.java
+++ src/backend/minijeu/JeuPassword.java
@@ -1,7 +1,26 @@
 package backend.minijeu;
 
+import backend.donnees.CSVReader;
+import backend.donnees.Password;
+
+import java.util.List;
+
 public class JeuPassword extends Minijeu {
+    List<Password> ListPasswords;
 
-    //Ajouter attribut ListePasswords<Password>
+    // Constructeur pour initialiser le JeuPassword avec les données recuperees du le fichir CSV
+    public JeuPassword(String filePath) {
+        ListPasswords = CSVReader.readCSVPassword(filePath);
+    }
 
+    // Methode pour test si le password a le niveau correcte
+    public boolean testPasswordLevel(String pLevel, int pNiveau) {
+        for (Password password : ListPasswords) {
+            // Assuming case-sensitive comparison for passwords
+            if (password.getmdp().equals(pLevel) && password.getlvl() == pNiveau) {
+                return true; // Password et son niveau correctes
+            }
+        }
+        return false; // Pasword et son niveau incorrectes
+    }
 }
