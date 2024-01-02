94727a2bce0a7f46239ceb9c2dadab91a5aab146
Senku89 <148382187+Senku89@users.noreply.github.com>
2024-01-02 20:10:50 +0100

    Correction semicolon innecesaire. Et ajout de Fenetre sur le main pour le lancer.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package src.main;
 
+import frontend.Fenetre;
+
+import javax.swing.*;
+
 import static src.backend.donnees.TestDonnees.TestCSV;
 
 public class Main
@@ -7,6 +11,10 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Static method");
-        TestCSV();
+        // TestCSV();
+        SwingUtilities.invokeLater(() -> {
+            Fenetre fenetre = new Fenetre();
+            // Initialization aditionnelles ici comme fenetre.setPanelActif(initialPanel);
+        });
     }
 }
