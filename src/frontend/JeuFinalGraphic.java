cc51acdb05ecac416f01e939381fc10507f283ff
Senku89 <148382187+Senku89@users.noreply.github.com>
2024-01-10 00:34:30 +0100

    Correction paths image sur Init et Variables en double sur JeuFinalGraphic

--- src/frontend/JeuFinalGraphic.java
+++ src/frontend/JeuFinalGraphic.java
@@ -20,16 +20,12 @@ public class JeuFinalGraphic extends MiniJeuGraphic{
 	JTextArea zoneIndices, zoneIndice1, zoneIndice2, zoneIndice3;
 	JTextField zoneMotFinal;
 	Bouton boutonValider;
-	JTextField zoneMotFinal;
-	Bouton boutonValider;
 
 	int spacingHeight = 20, hauteurZoneCentrale = 320;
-	int paddingWidth = 30, paddingHeight = 15, beetweenIntituleAndIndice = 10, indiceHeight = 25;
 	int paddingWidth = 30, paddingHeight = 15, beetweenIntituleAndIndice = 10, indiceHeight = 25;
 
 	Color foregroundIntituleIndice = Color.BLACK, backgroundIntituleIndice = new Color(0, 0, 0, 0);
 	Color foregroundIndiceTrouve = Color.BLACK, backgroundIndiceTrouve = new Color(225, 225, 240, 255);
-	Color foregroundIndicePasTrouve = Color.WHITE, backgroundIndicePasTrouve = new Color(50, 50, 50, 255);
 	Color foregroundIndicePasTrouve = Color.WHITE, backgroundIndicePasTrouve = new Color(50, 50, 50, 255);
 	Font fontIntituleIndice = new Font("Helvetica", Font.BOLD, 20), fontIndice = new Font("Helvetica", Font.BOLD, 15);
 
--- src/general/Init.java
+++ src/general/Init.java
@@ -4,12 +4,12 @@ import java.awt.Color;
 
 public class Init{
 	
-	public static String imagefondmenuprincipal = "../resources/images/menu.png";
-	public static String imagefondjeuquiz = "../resources/images/question.png";
-	public static String imagefondjeumdp = "../resources/images/mdp.png";
-	public static String imagefondjeudecrypt = "../resources/images/decryp.png";
-	public static String iconetimer = "../resources/images/stopwatch.png";
-	public static String tableaualphabetsymboles = "../resources/images/tableau.png";
+	public static String imagefondmenuprincipal = "resources/images/menu.png";
+	public static String imagefondjeuquiz = "resources/images/question.png";
+	public static String imagefondjeumdp = "resources/images/mdp.png";
+	public static String imagefondjeudecrypt = "resources/images/decryp.png";
+	public static String iconetimer = "resources/images/stopwatch.png";
+	public static String tableaualphabetsymboles = "resources/images/tableau.png";
 
 	public static Color couleurfondbouton = new Color(60, 60, 60);
 	public static Color couleurtextbouton = new Color(250, 250, 250);
@@ -19,6 +19,6 @@ public class Init{
 	public static String texteIntro = "Welcome Everyone";
 
 	public static String getCle(int num){
-		return "./resources/images/cle"+num+".png";
+		return "resources/images/cle"+num+".png";
 	}
 }
