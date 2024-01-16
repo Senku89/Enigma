7a25ab424cd9081313bcdc336120838582d21de6
RyuRikyu <randriamaro.thomas@gmail.com>
2024-01-16 20:26:50 +0100

    commit lanceJeu.sh pour possibilité rebase

--- lanceJeu.sh
+++ lanceJeu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-javac util/*.java && javac backend/donnees/*.java && javac backend/minijeu/*.java && javac general/*.java && javac backend/*.java && javac frontend/*.java && javac Main.java && java Main
+javac util/*.java && javac backend/donnees/*.java && javac backend/minijeu/*.java && javac general/*.java && javac frontend/*.java && javac Main.java && java Main
 
 rm Main.class;
 rm */*.class;
